dc4651892640b6df4f403b298159ccb19a83d7f4
Roskova <89421030+Roskova@users.noreply.github.com>
2022-04-20 19:20:34 -0400

    Ajout de l'autocalculation des années bissextile

--- PhotoDirectoryScriptByWeek.ps1
+++ PhotoDirectoryScriptByWeek.ps1
@@ -5,27 +5,23 @@ Write-Host "#   Cree une arborescence par semaine et mois           #"
 Write-Host "#   ~\[Annee]\[Mois]\Semaine du [start] au [end] [Mois] #"
 Write-Host "#   Script creer par ROSKOVA                            #"
 Write-Host "# # # # # # # # # # # # # # # # # # # # # # # # # # # # #"
-# Année à générer   
-$annee = 2022;
-# Jour du premier dimanche du mois 
-$debutSA = (2,6,6,3,1,5,3,7,4,2,6,4); 
-# Nombre de dimanche dans le mois
-#        J,F,M,A,M,J,J,A,S,O,N,D
-$nbSA = (5,4,4,4,5,4,5,4,4,5,4,4);
-# Nombre de jour en février        
-$nbFev = 28;
 read-host  "Appuyez sur ENTER pour lancer le script";
-$moisA = ("Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre");
-$NbJA = (31,$nbFev,31,30,31,30,31,31,30,31,30,31);
+# 
+# Nb de Dimanche par mois
+$nbSA = (5,4,4,4,5,4,5,4,4,5,4,4);
+# Année à générer et jour du premier dimanche de l'année
+$annee = 2022;
+$premierDimanche = 2;
+#Section librarie NE PAS ÉDITER
+$NbJA=(31,$nbFev,31,30,31,30,31,31,30,31,30,31);$debutSA = @($premierDimanche,0,0,0,0,0,0,0,0,0,0,0);$moisA = ("Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre");
+if($annee%4 -eq 0 -and ($annee % 100 -ne 0 -or $annee % 400 -ne 0)){$nbFev=29;}else{$nbFev=28;}for($f = 1; $f -lt $debutSA.Count; $f++){$debutSA[$f]=$debutSA[$f-1]+($nbSA[$f-1]*7)-$NbJA[$f-1];}
+#Section du script de génération
 mkdir "$annee";
 for ($j = 0; $j -le 11; $j++) {
-    [int]$nbM = $j; 
-    $mois = $moisA[$nbM];
-    $moisS = $moisA[$nbM+1];
-    $nbJ = $NbJA[$nbM];
-    [int]$debutS = $debutSA[$nbM];
-    [int]$finS = $debutS+6;
-    [int]$nbS = $nbSA[$nbM];
+    #Variable
+    [int]$nbM=$j;$mois=$moisA[$nbM];$moisS=$moisA[$nbM+1];$nbJ=$NbJA[$nbM];
+    [int]$debutS=$debutSA[$nbM];[int]$finS=$debutS+6;[int]$nbS=$nbSA[$nbM];
+    #Création de l'arborescence
     mkdir "$annee\$mois";
     for ($i = $nbS; $i -gt 0; $i--){
         if($nbM -eq 11){$MoisS = $moisA[0]}
