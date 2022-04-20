5882e1c823691eb1bc2eb72c617611e2a491db99
Roskova <89421030+Roskova@users.noreply.github.com>
2022-04-20 01:50:15 -0400

    Update PhotoDirectoryScriptByWeek.ps1

--- PhotoDirectoryScriptByWeek.ps1
+++ PhotoDirectoryScriptByWeek.ps1
@@ -19,32 +19,23 @@ $moisA = ("Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Sept
 $NbJA = (31,$nbFev,31,30,31,30,31,31,30,31,30,31);
 mkdir "$annee";
 for ($j = 0; $j -le 11; $j++) {
-  [int]$nbM = $j; 
-  $mois = $moisA[$nbM];
-  $moisS = $moisA[$nbM+1];
-  $nbJ = $NbJA[$nbM];
-  [int]$debutS = $debutSA[$nbM];
-  [int]$finS = $debutS+6;
-  [int]$nbS = $nbSA[$nbM];
-  mkdir "$annee\$mois";
-  for ($i = $nbS; $i -gt 0; $i--){
-    if($nbM -eq 11){
-      $MoisS = $moisA[0]
-    }if($debutS -gt $finS){
-      mkdir "$annee\$mois\Semaine du $debutS au 0$finS $moisS";
-    }elseif($debutS -lt 10 -and $finS -ge 10){
-      mkdir "$annee\$mois\Semaine du 0$debutS au $finS $mois";
-    }elseif($debutS -lt 10 -and $finS -lt 10){
-      mkdir "$annee\$mois\Semaine du 0$debutS au 0$finS $mois";
-    }elseif($debutS -ge 10 -and $finS -lt 10){
-      mkdir "$annee\$mois\Semaine du $debutS au 0$finS $mois";
-    }else{
-      mkdir "$annee\$mois\Semaine du $debutS au $finS $mois";
-    }if(($finS+7) -gt $nbJ){
-      $finS -=$nbJ;
+    [int]$nbM = $j; 
+    $mois = $moisA[$nbM];
+    $moisS = $moisA[$nbM+1];
+    $nbJ = $NbJA[$nbM];
+    [int]$debutS = $debutSA[$nbM];
+    [int]$finS = $debutS+6;
+    [int]$nbS = $nbSA[$nbM];
+    mkdir "$annee\$mois";
+    for ($i = $nbS; $i -gt 0; $i--){
+        if($nbM -eq 11){$MoisS = $moisA[0]}
+        if($debutS -gt $finS){mkdir "$annee\$mois\Semaine du $debutS au 0$finS $moisS";}
+        elseif($debutS -lt 10 -and $finS -ge 10){mkdir "$annee\$mois\Semaine du 0$debutS au $finS $mois";}
+        elseif($debutS -lt 10 -and $finS -lt 10){mkdir "$annee\$mois\Semaine du 0$debutS au 0$finS $mois";}
+        elseif($debutS -ge 10 -and $finS -lt 10){mkdir "$annee\$mois\Semaine du $debutS au 0$finS $mois";}
+        else{mkdir "$annee\$mois\Semaine du $debutS au $finS $mois";}
+        if(($finS+7) -gt $nbJ){$finS -=$nbJ;}
+        $finS += 7;$debutS +=7;
     }
-    $finS += 7;
-    $debutS +=7;
-  }
 }
 read-host "Creation terminer";
